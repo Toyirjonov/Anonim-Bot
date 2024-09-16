0e21ca5e79dd77ba12df2c5a6737376be45f9a50
Toyirjonov <144303564+Toyirjonov@users.noreply.github.com>
2024-09-16 16:15:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage  # Используем MemoryStorage
-from aiogram.utils.exceptions import Throttled  # Импортируем исключение Throttled
+from aiogram.contrib.fsm_storage.memory import MemoryStorage  
+from aiogram.utils.exceptions import Throttled  
 from datetime import datetime
 
-API_TOKEN = '7340059630:AAHy-ZSI7rL46SzrOM0yGtbdivo0ltZq-Lg'
+API_TOKEN = 'Your_Bot_Token'
 
-# Инициализация хранилища
-storage = MemoryStorage()  # Используем хранилище в памяти
 
-# Инициализация бота и диспетчера
+storage = MemoryStorage()
+
+
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot, storage=storage)  # Передаем хранилище в диспетчер
+dp = Dispatcher(bot, storage=storage) 
 
-# ID канала для пересылки сообщений
-CHANNEL_ID = '-4229504298'  # Укажи ID твоего канала
 
-# Приветственное сообщение
+CHANNEL_ID = 'Your_Chat_Id'  
+
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Я анонимный бот. Напиши что-нибудь, и я передам это анонимно.")
 
-# Логирование сообщений для админов
+
 async def log_message(message: types.Message):
     log_text = f"[{datetime.now()}] User ID {message.from_user.id} отправил сообщение: {message.text}"
-    logging.info(log_text)  # Логирование в консоль
+    logging.info(log_text)  
 
-# Фильтрация контента и антиспам
+
 @dp.message_handler(content_types=types.ContentType.ANY)
 async def handle_message(message: types.Message):
     try:
         # Ограничение по количеству сообщений (антиспам)
-        await dp.throttle('message', rate=2)  # Пример: 1 сообщение в минуту
+        await dp.throttle('message', rate=2)  
 
-        # Фильтрация по типам контента
+       
         if message.content_type == 'text':
             await bot.send_message(CHANNEL_ID, message.text)
             await message.reply("Твое текстовое сообщение отправлено анонимно!")
@@ -46,10 +46,9 @@ async def handle_message(message: types.Message):
         else:
             await message.reply("Поддерживаются только текст, фото и видео.")
 
-        # Логирование отправленного сообщения
         await log_message(message)
 
-    except Throttled as throttled:  # Ловим исключение Throttled
+    except Throttled as throttled: 
         await message.reply(f"Слишком много сообщений! Подожди {throttled.rate} секунд перед отправкой следующего.")
 
 if __name__ == '__main__':
